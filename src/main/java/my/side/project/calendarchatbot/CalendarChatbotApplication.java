65034b3ffe5ee0eaff0f78ac6979d02f3dac7071
vison <vison.li@verizonmedia.com>
2020-08-15 15:58:01 +0800

    init

---
+++ src/main/java/my/side/project/calendarchatbot/CalendarChatbotApplication.java
@@ -0,0 +1,12 @@
+package my.side.project.calendarchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarChatbotApplication.class, args);
+	}
+}
---
+++ src/test/java/my/side/project/calendarchatbot/CalendarChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package my.side.project.calendarchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
