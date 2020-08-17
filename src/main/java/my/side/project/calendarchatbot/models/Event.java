9fee0f519e5ead8ab096b4658555080f1358fd4c
vison <vison.li@verizonmedia.com>
2020-08-18 02:07:02 +0800

    use lombok instead of getter and setter

--- src/main/java/my/side/project/calendarchatbot/models/Event.java
+++ src/main/java/my/side/project/calendarchatbot/models/Event.java
@@ -1,58 +1,19 @@
 package my.side.project.calendarchatbot.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
 /**
  * the event model of firestore
  */
+@Getter
+@Setter
+@Accessors(chain = true)
 public class Event {
-
     String id;
     String title;
     String userId;
     String startTime;
     String endTime;
-
-    public String getId() {
-        return id;
-    }
-
-    public Event setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Event setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public Event setUserId(String userId) {
-        this.userId = userId;
-        return this;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public Event setStartTime(String startTime) {
-        this.startTime = startTime;
-        return this;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public Event setEndTime(String endTime) {
-        this.endTime = endTime;
-        return this;
-    }
 }
--- src/main/java/my/side/project/calendarchatbot/models/EventDataObject.java
+++ src/main/java/my/side/project/calendarchatbot/models/EventDataObject.java
@@ -1,10 +1,16 @@
 package my.side.project.calendarchatbot.models;
 
 import com.google.cloud.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 /**
  * the event model of firestore
  */
+@Getter
+@Setter
+@Accessors(chain = true)
 public class EventDataObject {
 
     String title;
@@ -13,58 +19,4 @@ public class EventDataObject {
     Timestamp endTime;
     Object createdAt;
     Object updatedAt;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public EventDataObject setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public EventDataObject setUserId(String userId) {
-        this.userId = userId;
-        return this;
-    }
-
-    public Timestamp getStartTime() {
-        return startTime;
-    }
-
-    public EventDataObject setStartTime(Timestamp startTime) {
-        this.startTime = startTime;
-        return this;
-    }
-
-    public Timestamp getEndTime() {
-        return endTime;
-    }
-
-    public EventDataObject setEndTime(Timestamp endTime) {
-        this.endTime = endTime;
-        return this;
-    }
-
-    public Object getCreatedAt() {
-        return createdAt;
-    }
-
-    public EventDataObject setCreatedAt(Object createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
-
-    public Object getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public EventDataObject setUpdatedAt(Object updatedAt) {
-        this.updatedAt = updatedAt;
-        return this;
-    }
 }
